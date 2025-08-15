1899f4b94faf8649288559f5f44ab56d8528a3d5
Dwarkesh <luxcruse2@gmail.com>
2025-08-15 12:19:03 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -1,79 +1,28 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
+$(document).ready(function() {
+  $(window).scroll(function() {
+    $('.scroll-up-btn').toggleClass('show', this.scrollY > 500);
+    $('.navbar').toggleClass('sticky', this.scrollY > 20);
+  });
 
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
+  $('.scroll-up-btn').click(function() {
+    $('html').animate({ scrollTop: 0 });
+    $('html').css("scrollBehavior", "auto");
+  });
 
-    $('.navbar .menu li a').click(function(){
-        $('html').css("scrollBehavior", "smooth");
-    });
-    $('.menu li a').click(function (e) {
-        e.preventDefault(); 
-        const target = $(this).attr('href'); // Get the target section ID
-        console.log('Clicked link:', target); // Debugging
-        if (target.startsWith('#')) {
-            $('html, body').animate({
-                scrollTop: $(target).offset().top
-            }, 800); 
-        }
-    });
+  $('.navbar .menu li a').click(function() {
+    $('html').css("scrollBehavior", "smooth");
+  });
 
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
+  $('.menu-btn').click(function() {
+    $('.navbar .menu').toggleClass('active');
+    $('.menu-btn i').toggleClass('active');
+  });
 
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        // strings: ["", "Dwarkesh Dubey ", ],
-        strings: ["", "Web Developer", "Software Engineer", "UI/UX Designer"],
-        typeSpeed: 80,
-        backSpeed: 40,
-        loop: true
-    });
+  new Typed(".typing", {
+    strings: ["Developer", "Designer", "Freelancer", "B.Tech Student"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
 
-    
-
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
 });
-// Smooth scroll to the Skills section
